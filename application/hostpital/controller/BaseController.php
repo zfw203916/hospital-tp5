8d784dbe1ec4eca5346a154a3212921f6771a615
zfw <15328628@qq.com>
2017-08-27 01:01:52 +0800

    add new

---
+++ application/hostpital/controller/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 8/27/2017
+ * Time: 12:49 AM
+ */
+
+namespace app\hostpital\BaseController;
+
+
+class BaseController
+{
+
+}
---
+++ application/hostpital/controller/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\hostpital\controller;
+
+class IndexController extends BaseController
+{
+    public function index()
+    {
+
+    }
+}
